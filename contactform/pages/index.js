e876109bc30c2de4b80ffa7b2bbb731e0aeb762d
Hank Pham <hkmpham147@gmail.com>
2021-09-09 17:12:11 -0400

    Basic layout with TailwindCSS

--- contactform/pages/index.js
+++ contactform/pages/index.js
@@ -10,6 +10,28 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
+      <div className="min-h-screen bg-white text-center">
+
+        <div className="">
+
+          {/* Right */}
+          <div className="bg-white rounded p-4">
+            <p>test3</p>
+            <p>test4</p>
+          </div>
+
+          {/* Left */}
+          <div className="bg-green-400 rounded p-4 bottom-1">
+            <h3 className="text-md text-white fond-bold">
+              Contact Information
+            </h3>
+          </div>
+
+        </div>
+          
+
+      </div>
+
       
     </div>
   )
